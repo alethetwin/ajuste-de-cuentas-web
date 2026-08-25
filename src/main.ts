33d3af1904c3f4810f26eaff4163aff34713440d
Alejandro Sánchez <alethetwin@icloud.com>
2026-08-25 14:27:59 -0600

    Primevue setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
 
+const licenseKey = import.meta.env.VITE_PRIMEVUE_LICENSE_KEY
+
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+  license: licenseKey,
+})
 app.use(createPinia())
 app.use(router)
 
